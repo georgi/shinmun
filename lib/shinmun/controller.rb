3721aa56c3954eb7c962ea8cb486b718d6770f4e
Matthias Georgi <matti.georgi@gmail.com>
2008-11-12 23:36:17 +0100

    New CommentController. Works with jQuery frontend.

--- lib/shinmun/controller.rb
+++ lib/shinmun/controller.rb
@@ -6,10 +6,17 @@ module Shinmun
 
     def initialize(blog)
       @blog = blog
-      @routing = [[/\.rss$/, FeedController],
-                  [/^\/\d\d\d\d\/\d+\//, PostController],
-                  [/^\/categories/, CategoryController],
-                  [//, PageController]]
+      @routing = []
+
+      map /\.rss$/, FeedController
+      map /^\/\d\d\d\d\/\d+\//, PostController
+      map /^\/categories/, CategoryController
+      map /^\/comments/, CommentsController
+      map //, PageController
+    end
+
+    def map(pattern, controller)
+      @routing << [pattern, controller]
     end
 
     def call(env)
@@ -17,11 +24,14 @@ module Shinmun
       response = Rack::Response.new
 
       @blog.reload
-      @blog.pack_assets
 
       klass = find_controller(request.path_info)
+
       controller = klass.new(@blog, request, response)
       controller.handle_request
+
+      response.status ||= 200
+      response.finish
     end
 
     def find_controller(path)
@@ -54,24 +64,25 @@ module Shinmun
 
     def handle_request
       action = request.request_method.downcase
-
       response.body = send(action) if self.class.public_instance_methods.include?(action)
-      response.status ||= 200
-      response.finish
     end
   end
 
   class PageController < Controller
     def get
-      page = blog.find_page(path.empty? ? 'index' : path) or raise "#{path} not found"
-      blog.render_page(page)
+      if path.empty?
+        blog.render_index_page
+      else
+        page = blog.find_page(path) or raise "#{path} not found"
+        blog.render_page(page)
+      end
     end
   end
 
   class PostController < PageController
     def get
       year, month, file = path.split('/')
-      if file == '' or file == 'index'
+      if file.empty?
         blog.render_month(year.to_i, month.to_i)
       else
         post = blog.find_post(path) or raise "#{path} not found"
@@ -85,7 +96,7 @@ module Shinmun
       path_list = path.split('/')
       case path_list[0]
       when 'categories'
-        category = blog.find_category(path_list[1])
+        category = blog.find_category(path_list[1].chomp('.rss'))
         blog.render_category_feed(category)
       when 'index'
         blog.render_index_feed
@@ -95,9 +106,26 @@ module Shinmun
 
   class CategoryController < Controller
     def get
-      category = blog.find_category(path.split('/')[1])
+      category = blog.find_category(path.split('/')[1].chomp('.html'))
       blog.render_category(category)
     end
+  end
+
+  class CommentsController < Controller
+    GUID_PATTERN = /^(\{{0,1}([0-9a-fA-F]){8}-([0-9a-fA-F]){4}-([0-9a-fA-F]){4}-([0-9a-fA-F]){4}-([0-9a-fA-F]){12}\}{0,1})$/ 
+
+    def post
+      params['guid'].match(GUID_PATTERN) or raise 'invalid guid'
+
+      comment = Comment.new(Time.now, params['name'], params['website'], params['text'])
+
+      Comment.write(params['guid'], comment)
+
+      comments = Comment.read(params['guid'])
+
+      blog.render_comments(comments)
+    end
+
   end
 
 end
