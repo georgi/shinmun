69a0683b563b69a07563a7a9824eba658d7d1b7b
Matthias Georgi <matti.georgi@gmail.com>
2008-11-12 23:35:29 +0100

    New cache mechanism for rack adaper. Aggregations like flickr. Changed file organization like templates and config files.

--- lib/shinmun/blog.rb
+++ lib/shinmun/blog.rb
@@ -1,109 +1,143 @@
 module Shinmun
 
-  # This class represents a blog. You need to provide a source
-  # directory and the meta file `blog.yml` which defines some variables.
-  # Example for `blog.yml`:
-  #    blog_title: Matthias Georgi
-  #    blog_description: Webdev, Gamedev, Interaction Design
-  #    blog_language: en
-  #    blog_author: Matthias Georgi
-  #    blog_url: http://www.matthias-georgi.de
-  #    categories:
-  #      - Ruby
-  #      - Emacs
   class Blog
 
-    FILE_PATTERN = '**/*.{md,tt,html,rhtml}'
+    # Define reader methods for configuration.
+    def self.config_reader(file, *names)
+      names.each do |name|
+        name = name.to_s        
+        define_method(name) { @config[file][name] }
+      end
+    end
 
-    attr_reader :meta, :posts, :pages
-    attr_reader :base_path, :images_path, :javascripts_path, :stylesheets_path, :repository
+    attr_reader :posts, :pages, :aggregations
+    
+    config_reader 'config/aggregations.yml', :audioscrobbler_feed, :delicious_feed, :flickr_feed, :fortythree_feed
+    config_reader 'config/assets.yml', :javascript_files, :stylesheet_files, :base_path, :images_path, :javascripts_path, :stylesheets_path
+    config_reader 'config/blog.yml', :title, :description, :language, :author, :url, :repository
+    config_reader 'config/categories.yml', :categories
 
+    # Initialize the blog, load the config file and write the index files.
     def initialize
       @uuid = UUID.new
-      @templates = {}
-      reload
+
+      @config = Cache.new do |file|
+        YAML.load(File.read(file))
+      end
+
+      @stylesheets = Cache.new
+
+      @templates = Cache.new do |file|
+        ERB.new(File.read(file))
+      end
+
+      @javascripts = Cache.new do |file|
+        script = File.read(file)
+        script = Packr.pack(script) if defined?(Packr)
+        "/* #{file} */\n #{script}\n"
+      end
+
+      @posts_cache = Cache.new do |file|
+        Post.new(:filename => file).load
+      end
+
+      @pages_cache = Cache.new do |file|
+        Post.new(:filename => file).load
+      end
+
+      Dir['pages/**/*'].each do |file|
+        @pages_cache.load(file) if File.file?(file) and file[-1, 1] != '~'
+      end
+
+      Dir['posts/**/*'].each do |file|
+        @posts_cache.load(file) if File.file?(file) and file[-1, 1] != '~'
+      end
+
+      @config.load('config/aggregations.yml')
+      @config.load('config/assets.yml')
+      @config.load('config/blog.yml')
+      @config.load('config/categories.yml')
+
+      load_aggregations
+
+      Thread.start do
+        sleep 300
+        load_aggregations
+      end
     end
 
-    # Read config and all posts from disk.
+    def load_aggregations
+      @aggregations = {
+        :audioscrobbler => audioscrobbler_feed && Audioscrobbler.new(audioscrobbler_feed),
+        :delicious      => delicious_feed && Delicious.new(delicious_feed),
+        :flickr         => flickr_feed && FlickrAggregation.new(flickr_feed),
+        :fortythree     => fortythree_feed && Fortythree.new(fortythree_feed)
+      }
+    end
+
+    # Reload config, assets and posts.
     def reload
-      @meta = YAML.load(File.read('config/blog.yml'))
-      @base_path = meta['base_path'] || ''
-      @images_path = meta['images_path'] || 'images'
-      @javascripts_path = meta['javascripts_path'] || 'javascripts'
-      @stylesheets_path = meta['stylesheets_path'] || 'stylesheets'
-      @repository = meta['blog_repository']
-
-      Dir.chdir('posts') do
-        @posts = Dir[FILE_PATTERN].map { |path| Post.new(self, path).load }
-        @posts = @posts.sort_by { |post| post.date }.reverse
+      if @config.dirty? || @templates.dirty?
+        @config.reload!
+        @templates.reload!
+        @posts_cache.reload!
+        @pages_cache.reload!        
+      else
+        @config.reload_dirty!
+        @templates.reload_dirty!
+        @posts_cache.reload_dirty!
+        @pages_cache.reload_dirty!
       end
 
-      Dir.chdir('pages') do
-        @pages = Dir[FILE_PATTERN].map { |path| Post.new(self, path).load }
-      end
+      @posts = @posts_cache.values.sort_by { |p| p.date }.reverse
+      @pages = @pages_cache.values
 
-      load 'templates/helpers.rb' if File.exist?('templates/helpers.rb')
+      pack_javascripts if @javascripts.dirty? or @javascripts.empty?
+      pack_stylesheets if @stylesheets.dirty? or @stylesheets.empty? 
+      link_assets
+
+      load 'templates/helpers.rb'
     end
 
-    def public_paths
-      ['admin', images_path, stylesheets_path, javascripts_path]
-    end
-
+    # Use rsync to synchronize the rendered blog to web server.
     def push
       system "rsync -avz public/ #{repository}"
     end
 
-    def url
-      @meta['blog_url']
+    def urlify(string)
+      string.downcase.gsub(/[ -]+/, '-').gsub(/[^-a-z0-9_]+/, '')
     end
 
-    def categories
-      meta['categories']
-    end
-
-    def compress(filename)
-      require 'packr'
-      src = File.read(filename)
-      name, ext = filename.split('.')
-      min = name + '.min.' + ext
-      if !File.exist?(min) or File.mtime(filename) > File.mtime(min)
-        open(min, "wb") do |file|
-          file << Packr.pack(src)
-        end
-      end
-      File.read(min)
-    end
-
+    # Compress the javascripts using PackR and write them to one file called 'all.js'.
     def pack_javascripts
       File.open("assets/#{javascripts_path}/all.js", "wb") do |io|
-        for file in meta['pack_javascripts']
-          io << compress("assets/#{javascripts_path}/#{file.strip}.js") << "\n\n"
+        for file in javascript_files
+          io << @javascripts["assets/#{javascripts_path}/#{file.strip}.js"] << "\n\n"
         end
       end
     end
 
+    # Pack the stylesheets and write them to one file called 'all.css'.
     def pack_stylesheets
       File.open("assets/#{stylesheets_path}/all.css", "wb") do |io|
-        for file in meta['pack_stylesheets']
-          io << File.read("assets/#{stylesheets_path}/#{file.strip}.css") << "\n\n"
+        for file in stylesheet_files
+          io << @stylesheets["assets/#{stylesheets_path}/#{file.strip}.css"] << "\n\n"
         end
       end
     end
 
-    def copy_assets
-      FileUtils.cp_r 'assets/.', 'public'
-    end
-    
-    def pack_assets
-      pack_javascripts if meta['pack_javascripts']
-      pack_stylesheets if meta['pack_stylesheets']
+    # Link assets into public folder.
+    def link_assets
+      Dir['assets/*'].each do |file|
+        FileUtils.ln_sf(file, file.sub(/^assets/, 'public'))
+      end
     end
 
     # Write a file to output directory.
     def write_file(path, data)
       FileUtils.mkdir_p("public/#{base_path}/#{File.dirname path}")
-      filepath = "public/#{base_path}/#{path}"
-      open(filepath, 'wb') do |file|
+
+      open("public/#{base_path}/#{path}", 'wb') do |file|
         file << data
       end    
     end
@@ -115,7 +149,8 @@ module Shinmun
 
     # Return all posts in given category.
     def posts_for_category(category)
-      posts.select { |p| p.category == category }
+      name = category['name']
+      posts.select { |p| p.category == name }
     end
 
     # Return all posts with any of given tags.
@@ -134,15 +169,17 @@ module Shinmun
       posts.map { |p| [p.year, p.month] }.uniq.sort
     end
 
+    # Create a new post with given title. Date and guid will be set
+    # automatically.
     def create_post(title)
       date = Date.today
-      name = Shinmun.urlify(title)
+      name = urlify(title)
       path = "#{date.year}/#{date.month}/#{name}.md"
 
       if File.exist?(file)
         raise "#{file} exists"
       else
-        post = Post.new(self, path)
+        post = Post.new(:path => path)
         post.title = title
         post.head = {
           'date' => date,
@@ -162,75 +199,86 @@ module Shinmun
     end
 
     def find_category(category)
-      category = Shinmun.urlify(category)
-      categories.find { |c| Shinmun.urlify(c) == category }
-    end
-
-    # Return template variables as hash.
-    def variables
-      meta.merge(:posts => posts, 
-                 :months => months,
-                 :categories => categories)
-    end
-
-    # Read and cache template file.
-    def template(name)
-      ERB.new(File.read("templates/#{name}"))
+      category = urlify(category)
+      categories.find { |c| urlify(c['name']) == category }
     end
 
     # Render template with given variables.
     def render_template(name, vars)
-      template = Template.new(template(name), self)
+      template = Template.new(@templates["templates/#{name}"], name)
       template.set_variables(vars)
       template.render
     end
 
     def render_layout(vars)
-      render_template("layout.rhtml", variables.merge(vars))
+      render_template("layout.rhtml", vars.merge(:blog => self))
     end
 
     # Render named template and insert into layout with given variables.
     def render(name, vars)
-      vars = variables.merge(vars)
-      render_layout(vars.merge(:content => render_template(name, vars)))
+      render_layout(vars.merge(:content => render_template(name, vars.merge(:blog => self))))
     end
 
+    # Render given post using the post template and the layout template.
     def render_post(post)
       render('post.rhtml', post.variables.merge(:header => post.category))
     end
 
+    # Render given page using only the layout template.
     def render_page(page)
       render_layout(page.variables.merge(:content => page.body_html))
     end
 
-    def render_category(category)
-      posts = posts_for_category(category)
-      render("list.rhtml",  
-             :header => category,
-             :category => category, 
+    # Render comments.
+    def render_comments(comments)
+      render_template('comments.rhtml', :comments => comments)
+    end
+
+    # Render index page using the index and the layout template.
+    def render_index_page
+      render('index.rhtml', 
+             :header => 'Home',
              :posts => posts)
     end
 
+    # Render the category summary for given category.
+    def render_category(category)
+      posts = posts_for_category(category)
+      render("category.rhtml",  
+             :header => category['name'],
+             :category => category,
+             :posts => posts)
+    end
+
+    # Render the archive summary for given month.
     def render_month(year, month)
       path = "#{year}/#{month}"
       month_name = Date::MONTHNAMES[month]
       posts = posts_for_month(year, month)
-      render("list.rhtml", 
+      render("month.rhtml", 
              :header => "#{month_name} #{year}",
              :year => year, 
              :month => month_name, 
              :posts => posts)
     end
 
+    # Render index feed using the feed template.
     def render_index_feed
-      render_template("feed.rxml", variables)
+      render_template("index.rxml", 
+                      :blog => self,
+                      :posts => posts)
     end
 
+    # Render category feed for given category using the feed template .
     def render_category_feed(category)
-      posts = posts_for_category(category)
-      vars = variable.merge(:category => category, 
-                            :posts => posts)
-      render_template("feed.rxml", vars)
+      render_template("category.rxml", 
+                      :blog => self,
+                      :category => category, 
+                      :posts => posts_for_category(category))
+    end
+
+    def write_index_page
+      write_file("index.html", render_index_page)
     end
 
     # Write all pages.
@@ -257,7 +305,7 @@ module Shinmun
     # Write category summaries.
     def write_categories
       for category in categories
-        write_file("categories/#{Shinmun.urlify category}.html", render_category(category))
+        write_file("categories/#{urlify category['name']}.html", render_category(category))
       end
     end
 
@@ -265,14 +313,16 @@ module Shinmun
     def write_feeds
       write_file("index.rss", render_index_feed)      
       for category in categories
-        write_file("categories/#{Shinmun.urlify category}.rss", render_category(category))
+        write_file("categories/#{urlify category['name']}.rss", render_category_feed(category))
       end
     end
 
+    # Render everything to public folder.
     def write_all
-      pack_assets
-      copy_assets
+      load_aggregations
+      reload
 
+      write_index_page
       write_pages
       write_posts
       write_archives
