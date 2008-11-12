e34ac54b259816017835cb7b55a5d54e42c5d075
Matthias Georgi <matti.georgi@gmail.com>
2008-11-12 23:38:16 +0100

    Moved the helpers in to their own module.

--- lib/shinmun/template.rb
+++ lib/shinmun/template.rb
@@ -1,4 +1,3 @@
-
 module Shinmun
 
   module Helpers
@@ -12,9 +11,9 @@ module Shinmun
     attr_reader :erb, :blog
 
     # Initialize this template with an ERB instance.
-    def initialize(erb, blog)
+    def initialize(erb, file)
       @erb = erb
-      @blog = blog
+      @file = file
     end    
 
     # Set instance variable for this template.
@@ -28,73 +27,11 @@ module Shinmun
     # Render this template.
     def render
       @erb.result(binding)
-    end
-
-    # Render a hash as attributes for a HTML tag. 
-    def attributes(attributes)
-      attributes.map { |k, v| %Q{#{k}="#{v}"} }.join(' ')
-    end
-
-    # Render a HTML tag with given name. 
-    # The last argument specifies the attributes of the tag.
-    # The second argument may be the content of the tag.
-    def tag(name, *args)
-      text, attributes = args.first.is_a?(Hash) ? [nil, args.first] : args
-      "<#{name} #{attributes(attributes)}>#{text}</#{name}>"
-    end
-
-    # Render stylesheet link tag
-    def stylesheet_link_tag(*names)
-      names.map { |name|
-        mtime = File.mtime("assets/#{blog.stylesheets_path}/#{name}.css").to_i
-        path = "/#{blog.stylesheets_path}/#{name}.css?#{mtime}"
-        tag :link, :href => path, :rel => 'stylesheet', :media => 'screen'
-      }.join("\n")
-    end
-
-    # Render javascript tag
-    def javascript_tag(*names)
-      names.map { |name|
-        mtime = File.mtime("assets/#{blog.javascripts_path}/#{name}.js").to_i
-        path = "/#{blog.javascripts_path}/#{name}.js?#{mtime}"
-        tag :script, :src => path, :type => 'text/javascript'
-      }.join("\n")
-    end
-
-    # Render an image tag
-    def image_tag(file, options = {})
-      mtime = File.mtime("assets/#{blog.images_path}/#{file}").to_i
-      path = "/#{blog.images_path}/#{file}?#{mtime}"
-      tag :img, options.merge(:src => path)
-    end
-
-    # Render a link
-    def link_to(text, path, options = {})
-      tag :a, text, options.merge(:href => path)
-    end
-
-    # Render a link to a post
-    def post_link(post)
-      link_to post.title, "#{blog.base_path}/#{post.path}.html"
-    end
-
-    # Render a link to an archive page.
-    def archive_link(year, month)
-      link_to "#{Date::MONTHNAMES[month]} #{year}", "#{blog.base_path}/#{year}/#{month}/index.html"
-    end
-
-    # Render a date or time in a nice human readable format.
-    def date(time)
-      "%s %d, %d" % [Date::MONTHNAMES[time.month], time.day, time.year]
-    end
-
-    # Render a date or time in rfc822 format. This will be used for rss rendering.
-    def rfc822(time)
-      time.strftime("%a, %d %b %Y %H:%M:%S %z")
-    end
-
-    def strip_tags(html)
-      Shinmun.strip_tags(html)
+    rescue => e
+      e.backtrace.each do |s|
+        s.gsub!('(erb)', @file)
+      end
+      raise e
     end
 
   end
